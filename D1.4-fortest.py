c33e98e7ba2a16becbe0cad2bb46b4fdea1f9226
BenHsu20040424 <78331260+BenHsu20040424@users.noreply.github.com>
2021-02-01 17:05:46 +0800

    Add files via upload

---
+++ D1.4-fortest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:24:39 2021
+
+@author: MTAEXAM
+"""
+
+for n in range(1,10):
+    for m in range(1,10):
+        i = n*m
+        print(i)
+A = int(input(""))
+total = 1
+for A in range(1,A + 1):
+    total *= A
+    A += 1
+print("!=")
