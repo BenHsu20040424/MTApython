6fe0b4dcd3516ccef3a22b2bf1a9b35b4b6962a0
BenHsu20040424 <78331260+BenHsu20040424@users.noreply.github.com>
2021-02-01 10:42:33 +0800

    Add files via upload

---
+++ D1.2test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:24 2021
+
+@author: MTAEXAM
+"""
+
+top = float(input("上底："))
+buttom = float(input("下底："))
+height = float(input("高："))
+area = (top + buttom)*height/2
+print("梯形面積："+str(area))
