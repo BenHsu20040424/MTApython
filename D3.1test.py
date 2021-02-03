cc094beb353ed3386d52a4f5c4b081a245a67f9b
BenHsu20040424 <78331260+BenHsu20040424@users.noreply.github.com>
2021-02-03 17:01:25 +0800

    Add files via upload

---
+++ D3.1test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 15:22:29 2021
+
+@author: MTAEXAM
+"""
+
+f = open('file1.txt','r')
+for line in f:
+    print(line)
+f.close()
+with open('file1.txt','r') as f:
+    for line in f:
+        print(line)
